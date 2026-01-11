087e5d89016d66a63f5de3faeec6ae34876a22d3
rusdyahmad <rusdyahmad@users.noreply.github.com>
2026-01-11 14:51:08 +0800

    Add SSR prerender for SEO

---
+++ src/entry-server.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { StaticRouter } from "react-router-dom/server";
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App.jsx";
+import "./styles.css";
+
+export function render(url) {
+  const helmetContext = {};
+  const app = renderToString(
+    <HelmetProvider context={helmetContext}>
+      <StaticRouter location={url}>
+        <App />
+      </StaticRouter>
+    </HelmetProvider>
+  );
+
+  return { app, helmet: helmetContext.helmet };
+}
